918cd3c7bf7846cd836c2790d0c216e31eaaa6b9
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2021-08-29 12:40:31 -0500

    bar char axis added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@
  
  const width = 960;
  const height = 500;
+ const margin = {top:20, right:20, bottom:20, left:200}
  
  const csvUrl = "https://gist.githubusercontent.com/nafiul-nipu/e78beffe3c5e9306b93d3a8c1dfd1164/raw/UN_Population.csv"
  
 
- 
  function App() {
    const [data, setData] = useState(null)
  
@@ -27,22 +27,55 @@
      return <pre>Loading ...</pre>
    }
 
+   const innerHeight = height - margin.top - margin.bottom
+   const innerWidth = width - margin.left - margin.right
+
+
    const yScale = scaleBand()
                     .domain(data.map(d => d.Country))
-                    .range([0, height])
+                    .range([0, innerHeight])
 
    const xScale = scaleLinear()
                     .domain([0, max(data, d=> d.Population)])
-                    .range([0, width])
+                    .range([0, innerWidth])
  
    return (
      <svg width={width} height={height}>
-       {data.map(d => <rect 
-          x={0}
-          y={yScale(d.Country)}
-          width={xScale(d.Population)}
-          height={yScale.bandwidth()}
-       />)}
+       <g transform={`translate(${margin.left}, ${margin.top})`}>
+        {xScale.ticks().map(tickeValue => (
+          <g key={tickeValue} transform={`translate(${xScale(tickeValue)},0)`}>
+            <line
+              y2={innerHeight}
+              stroke='black'
+            />
+            <text 
+              y={innerHeight + 5}
+              dy={"0.71em"}
+              style={{textAnchor:'middle'}}
+            >{tickeValue}</text>
+          </g>
+        ))}
+
+        {yScale.domain().map(tickeValue => (          
+            <text 
+             key={tickeValue}
+             y={yScale(tickeValue) + yScale.bandwidth() / 2}
+             style={{textAnchor:'end'}}
+             x={-3}
+             dy={'0.32em'}
+            >{tickeValue}</text>
+         
+        ))}
+
+
+        {data.map(d => <rect 
+            key={d.Country}
+            x={0}
+            y={yScale(d.Country)}
+            width={xScale(d.Population)}
+            height={yScale.bandwidth()}
+        />)}
+       </g>
      </svg>
    );
  
