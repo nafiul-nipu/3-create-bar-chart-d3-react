1d7c64131d17320fd9481ea9e9e09948ca010f55
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2021-08-29 15:30:27 -0500

    rotate text

--- src/components/AxisLeft.js
+++ src/components/AxisLeft.js
@@ -2,10 +2,11 @@ export const AxisLeft = ({yScale}) => yScale.domain().map(tickeValue => (
     <g className='tick'>
       <text 
       key={tickeValue}
-      y={yScale(tickeValue) + yScale.bandwidth() / 2}
+      // y={yScale(tickeValue) + yScale.bandwidth() / 2}
       style={{textAnchor:'end'}}
-      x={-3}
+      // x={-3}
       dy={'0.32em'}
+      transform={`translate(-3, ${yScale(tickeValue) + yScale.bandwidth() / 2})rotate(-45)`}
       >{tickeValue}</text>
     </g>
   ));
